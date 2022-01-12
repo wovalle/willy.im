3cdb19f024dff50803e5df0e3d4cc3fe00f28646
Willy Ovalle <willyovalle16@gmail.com>
2022-01-12 11:32:07 +0100

    Fix build errors

--- src/pages/s/_middleware.ts
+++ src/pages/s/_middleware.ts
@@ -4,7 +4,7 @@ import { registerMiddleware } from "../../shorty/middleware"
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   return registerMiddleware(req, ev, {
-    onUrlCreate: async (url, data) => {
+    onUrlCreate: async (_url, _data) => {
       // await setKey(url, data)
       throw new Error("Not implemented")
     },
