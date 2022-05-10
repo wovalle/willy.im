5ab242b18d2269364c31518a2b7b8b97c14e1753
Willy Ovalle <willyovalle16@gmail.com>
2022-05-10 18:36:37 +0200

    components/Layout -> components/layouts/default

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,10 +1,10 @@
-import Layout from "../components/Layout"
+import Layout from "../components/layouts/Default"
 
 const Custom500 = () => {
   return (
     <Layout title="404 | Willy Ovalle" withFooterDivider={false}>
       <div className="flex flex-col items-center">
-        <div className="text-4xl text-title">oops... 500</div>
+        <div className="text-title text-4xl">oops... 500</div>
         <div className="text-subtitle">i probably messed up mate</div>
       </div>
     </Layout>
