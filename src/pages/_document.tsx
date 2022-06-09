bb11c7ede8e288cb39f8d1606c78ef024a66117a
Willy Ovalle <willyovalle16@gmail.com>
2022-06-09 22:10:28 +0200

    Editing images

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,10 @@ class NextDocument extends Document {
           <meta name="keywords" content="willy,ovalle,willyovalle,wovalle,personal,site,home" />
           <meta property="og:image" content="/android-chrome-512x512.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-16x16.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="yandex-verification" content="6ef5628ffe37d921" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#519ed1" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
