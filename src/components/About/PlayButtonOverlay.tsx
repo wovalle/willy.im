512d243c6e84f6290242b91261dfdceae3931b8e
Willy Ovalle <willyovalle16@gmail.com>
2022-07-11 00:06:56 +0200

    Renaming files after applying git ignorecase=false

---
+++ src/components/About/PlayButtonOverlay.tsx
@@ -0,0 +1,64 @@
+import { IconPlayerPause, IconPlayerPlay } from "@tabler/icons"
+import clsx from "clsx"
+import { FC, ReactNode } from "react"
+import { useAudioPlayer } from "../../hooks"
+import { ClientOnly } from "../ClientOnly"
+
+type PlayButtonOverlayProps = {
+  diameter: number
+  stroke?: number
+  children: ReactNode
+  audioUrl?: string
+  isHovered: boolean
+}
+
+export const PlayButtonOverlay: FC<PlayButtonOverlayProps> = ({
+  stroke = 4,
+  children,
+  audioUrl,
+  diameter,
+  isHovered,
+}) => {
+  const audio = useAudioPlayer(audioUrl)
+
+  const radius = diameter / 2
+  const normalizedRadius = radius - stroke * 2
+  const circumference = normalizedRadius * 2 * Math.PI
+  const strokeDashoffset = circumference - (audio.progress / 100) * circumference
+
+  const Icon = audio.playing ? IconPlayerPause : IconPlayerPlay
+
+  return (
+    <ClientOnly>
+      <div
+        className="relative cursor-pointer"
+        onClick={() => audio.toggle()}
+        style={{ width: diameter, height: diameter }}
+      >
+        <div
+          className={clsx("absolute left-0 top-0 rounded-xl", {
+            "opacity-50": !isHovered && !audio.playing,
+            "bg-black/50": isHovered || audio.playing,
+          })}
+        >
+          <div className="absolute" style={{ top: radius - 10, left: radius - 10 }}>
+            <Icon size={20} className="stroke-white" />
+          </div>
+          <svg height={diameter} width={diameter}>
+            <circle
+              className={clsx("stroke-white")}
+              fill="transparent"
+              strokeWidth={stroke}
+              strokeDasharray={circumference + " " + circumference}
+              style={{ strokeDashoffset, transform: "rotate(-90deg)", transformOrigin: "50% 50%" }}
+              r={normalizedRadius}
+              cx={radius}
+              cy={radius}
+            />
+          </svg>
+        </div>
+        {children}
+      </div>
+    </ClientOnly>
+  )
+}
