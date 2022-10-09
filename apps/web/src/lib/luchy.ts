0002755bae23376c12454da15bb2309f59b7ce4f
Willy Ovalle <willyovalle16@gmail.com>
2022-10-09 14:19:27 +0200

    feat: changes to butler

--- apps/web/src/lib/luchy.ts
+++ apps/web/src/lib/luchy.ts
@@ -4,7 +4,7 @@ import { ILogger } from "@luchyio/next"
 export const baseUrl =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://willy.im"
 
-export const basePath = "/api/v1/luchy"
+export const basePath = "/api/luchy"
 
 export const logger: ILogger = {
   debug: (...params) => process.env.NODE_ENV === 'development' ? console.log(...params) : () => {},
---
+++ apps/web/src/pages/api/butler/[[...path]].ts
@@ -0,0 +1,7 @@
+import { nextApiHandler } from "@willyim/butler"
+
+export default nextApiHandler()
+
+export const config = {
+  runtime: "nodejs",
+}
