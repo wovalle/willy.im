cfb9f7d398c3631f969d5aa50756e67e7dcb6de4
Willy Ovalle <willyovalle16@gmail.com>
2022-10-15 12:13:10 +0200

    fix: vercel url

--- apps/web/src/lib/luchy.ts
+++ apps/web/src/lib/luchy.ts
@@ -1,13 +1,17 @@
 import { ILogger } from "@luchyio/next"
 
-// TODO: can we do better?
+const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+
 export const baseUrl =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://willy.im"
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : `https://${vercelUrl ?? "willy.im"}`
 
 export const basePath = "/api/luchy"
 
 export const logger: ILogger = {
-  debug: (...params) => process.env.NODE_ENV === 'development' ? console.log(...params) : () => {},
+  debug: (...params) =>
+    process.env.NODE_ENV === "development" ? console.log(...params) : () => {},
   warn: (...params) => console.warn("[warn]", ...params),
   error: (...params) => console.error("[error]", ...params),
   info: (...params) => console.info("[info]", ...params),
