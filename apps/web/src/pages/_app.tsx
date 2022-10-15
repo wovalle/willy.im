6f86d45a831d5fb8a7829b769f4e05b0895f52ca
Willy Ovalle <willyovalle16@gmail.com>
2022-10-15 12:26:45 +0200

    fix: vercel url in client

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,16 +3,11 @@ import { ThemeProvider } from "next-themes"
 import { AppProps } from "next/app"
 import { ReactNode, useRef, useState } from "react"
 import { AudioPlayerContext } from "../hooks/useAudioPlayer"
+import { baseUrl } from "../lib/luchy"
 
 import "../styles/global.css"
 
 export { reportWebVitals } from "next-axiom"
-
-// TODO: Can we do better?
-const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api/luchy"
-    : "https://willy.im/api/luchy"
 
 export const AudioPlayerProvider = ({ children }: { children: ReactNode }) => {
   const [currentUrl, setCurrentUrl] = useState<string | undefined>()
@@ -29,7 +24,7 @@ export const AudioPlayerProvider = ({ children }: { children: ReactNode }) => {
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
-      <LuchyNextProvider baseUrl={baseUrl}> 
+      <LuchyNextProvider baseUrl={baseUrl}>
         <AudioPlayerProvider>
           <Component {...pageProps} />
         </AudioPlayerProvider>
