ca3286a794dcfd2ef208f72d7fcad51e8ccb80ef
Willy Ovalle <willyovalle16@gmail.com>
2022-10-31 21:09:51 +0100

    feat: adding deployment Url

--- apps/web/src/lib/luchy.ts
+++ apps/web/src/lib/luchy.ts
@@ -1,11 +1,11 @@
 import { ILogger } from "@luchyio/next"
 
-const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+const deploymentUrl = process.env.NEXT_PUBLIC_DEPLOYMENT_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
 
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${vercelUrl ?? "willy.im"}`
+    : `https://${deploymentUrl ?? "willy.im"}`
 
 export const basePath = "/api/luchy"
 
