3f1a315736ce3ea50110d857821899fc4c8f3ddb
Willy Ovalle <willyovalle16@gmail.com>
2022-11-03 21:11:36 +0100

    adding env var to debug

--- apps/web/src/lib/luchy.ts
+++ apps/web/src/lib/luchy.ts
@@ -11,7 +11,9 @@ export const basePath = "/api/luchy"
 
 export const logger: ILogger = {
   debug: (...params) =>
-    process.env.NODE_ENV === "development" ? console.log(...params) : () => {},
+    process.env.NODE_ENV === "development" || process.env.DEBUG
+      ? console.log("debug", ...params)
+      : () => {},
   warn: (...params) => console.warn("[warn]", ...params),
   error: (...params) => console.error("[error]", ...params),
   info: (...params) => console.info("[info]", ...params),
