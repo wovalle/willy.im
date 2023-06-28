543ad4ebd6e9be996760ebce28ef9dea8d4dc7e4
Willy Ovalle <willyovalle16@gmail.com>
2023-06-28 22:57:25 +0200

    Add redirect

--- packages/butler/src/index.ts
+++ packages/butler/src/index.ts
@@ -1,8 +1,10 @@
 import { mergeRouteHandlers } from "@willyim/common"
+import { handler as handleRedirect } from "./handleRedirect"
 import { handler as handleTelegramMessage } from "./handleTelegramMessage"
 import { handler as handleTelegramReminder } from "./handleTelegramReminder"
 
 export const handler = mergeRouteHandlers({
+  "/r": handleRedirect,
   "/telegram/message": handleTelegramMessage,
   "/telegram/reminder": handleTelegramReminder,
 })
