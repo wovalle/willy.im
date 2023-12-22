dbcf6f2af5c575dd66fa9d7733f74f5b3b6025f7
Willy Ovalle <willyovalle16@gmail.com>
2023-12-22 11:55:50 -0400

    fix doble script tag

--- apps/foo/app/root.tsx
+++ apps/foo/app/root.tsx
@@ -17,11 +17,11 @@ export default function App() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-K56XWQQ');</script>
+                })(window,document,'script','dataLayer','GTM-K56XWQQ');
                 `,
           }}
         ></script>
