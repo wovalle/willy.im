afc50bbe15f0734a227b31a7d386cf7ac8a91a4e
Willy Ovalle <willyovalle16@gmail.com>
2025-10-04 17:45:03 -0400

    /login

--- apps/www/app/routes.ts
+++ apps/www/app/routes.ts
@@ -4,7 +4,7 @@ export default [
   index("routes/index/index.page.tsx"),
   route("posts", "routes/posts/route.tsx"),
   route("posts/:slug", "routes/posts/posts.$slug.tsx"),
-  route("auth/login", "routes/auth/login.tsx"),
+  route("login", "routes/auth/login.tsx"),
   route("auth/*", "routes/auth/auth.$.ts"),
   route("cron/update", "routes/cron/update.ts"),
   route("about", "routes/about.tsx"),
