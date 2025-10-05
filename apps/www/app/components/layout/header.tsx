66fc09b6121e147482ff176af5894fcfea5727b9
Willy Ovalle <willyovalle16@gmail.com>
2025-10-05 10:21:31 -0400

    bye dark theme

--- apps/www/app/components/layout/header.tsx
+++ apps/www/app/components/layout/header.tsx
@@ -1,13 +1,9 @@
-import { IconMoon, IconSun } from "@tabler/icons-react"
 import { Link, useLocation } from "react-router"
 
-import { useState } from "react"
 import { cn } from "~/lib/cn"
 import { navLinks } from "~/static"
 
 export const Header = () => {
-  const [theme, setTheme] = useState<"light" | "dark">("light")
-
   const location = useLocation()
 
   const currentPathIsActive = (url: string) => {
@@ -30,28 +26,9 @@ export const Header = () => {
       </Link>
     ))
 
-  const themeIcon =
-    theme === "dark" ? (
-      <IconSun size="1.5em" className="hover:text-stone-600" />
-    ) : (
-      <IconMoon size="1.5em" className="hover:text-stone-600" />
-    )
-
   return (
     <header className="flex w-full max-w-4xl flex-row items-center justify-between self-center p-6">
       <nav className="flex gap-10">{links}</nav>
-
-      <button
-        type="button"
-        data-luchy-event="toggle-theme-pressed"
-        data-luchy-event-data={theme}
-        onClick={() => {
-          setTheme((theme) => (theme === "dark" ? "light" : "dark"))
-        }}
-        className="md:block"
-      >
-        {themeIcon}
-      </button>
     </header>
   )
 }
