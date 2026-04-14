49f95e8db5e5dca6552d2b25cec8ba35d76f496a
Willy Ovalle <willyovalle16@gmail.com>
2026-04-14 17:03:32 -0400

    Add .gitignore for rbac package, remove dist from tracking

--- packages/rbac/dist/src/create-permissions-hook.d.ts
+++
@@ -1,6 +0,0 @@
-export type UsePermissionsReturn<P extends string> = {
-    has(permission: P): boolean;
-    granted: P[];
-};
-export declare function createPermissionsHook<P extends string>(useGranted: () => P[]): () => UsePermissionsReturn<P>;
-//# sourceMappingURL=create-permissions-hook.d.ts.map
--- packages/rbac/dist/test/define-permissions.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=define-permissions.test.d.ts.map
